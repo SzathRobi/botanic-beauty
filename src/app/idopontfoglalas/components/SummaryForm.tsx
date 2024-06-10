fa37a0f142cc05b90724de43f8cca2ec61286785
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-06-10 14:36:01 +0200

    feat: summary form design

--- src/app/idopontfoglalas/components/SummaryForm.tsx
+++ src/app/idopontfoglalas/components/SummaryForm.tsx
@@ -1,4 +1,7 @@
 import { ContactInfo, Service } from "@prisma/client";
+import { PiScissors } from "react-icons/pi";
+import { RiCalendarScheduleLine } from "react-icons/ri";
+import { SlUserFemale } from "react-icons/sl";
 
 type SummaryFormProps = {
   choosenServices: Service[];
@@ -17,39 +20,61 @@ const SummaryForm = ({
 }: SummaryFormProps) => {
   return (
     <div>
-      <p className="text-xl text-center mb-8">
+      <p className="text-2xl text-center mb-16">
         Az időpontodat sikeresen felvettük!
       </p>
 
-      <div>
-        <p className="font-medium">Szolgáltatások:</p>
-        <div>
-          {choosenServices.map((service) => (
-            <p key={service.name}>
-              {service.name} ({service.duration} perc)
+      <div className="mb-24 flex flex-col md:flex-row items-start justify-evenly gap-8">
+        <div className="flex gap-4">
+          <PiScissors size={48} />
+          <div>
+            <p>Szolgáltatások:</p>
+            <div>
+              {choosenServices.map((service) => (
+                <p key={service.name} className="font-medium text-lg">
+                  {service.name} ({service.duration} perc)
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="flex gap-4">
+          <SlUserFemale size={48} />
+          <div>
+            <p>Fordrász:</p>
+            <p className="font-medium text-lg">{choosenHairdresser}</p>
+          </div>
+        </div> */}
+
+        <div className="flex gap-4">
+          <RiCalendarScheduleLine size={48} />
+          <div>
+            <p>Időpont:</p>
+            <p className="font-medium text-lg">
+              {selectedDate.toLocaleDateString()} {selectedTimeSlot}
             </p>
-          ))}
+          </div>
         </div>
       </div>
 
-      <div>
-        <p className="font-medium">Fordrász:</p>
-        <p>{choosenHairdresser}</p>
-      </div>
-
-      <div>
-        <p className="font-medium">Időpont:</p>
-        <p>
-          {selectedDate.toLocaleDateString()} {selectedTimeSlot}
-        </p>
-      </div>
-
-      <div>
-        <p className="font-medium">Elérhetőség:</p>
+      {/* <div>
+        <p className="font-medium text-lg">Elérhetőség:</p>
         <p>{contactInfo.name}</p>
         <p>{contactInfo.email}</p>
         <p>{contactInfo.phone}</p>
         {contactInfo.otherInfo && <p>{contactInfo.otherInfo}</p>}
+      </div> */}
+
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <p className="text-center">
+          Email címedre kiküldtünk egy visszaigazoló levelet.
+        </p>
+        <p className="text-red-500 text-center">
+          Ha mégse lenne jó az időpont, kérlek időben jelezd ezen az
+          elérhetőségen:
+        </p>
+        <p>+364206869</p>
       </div>
     </div>
   );
