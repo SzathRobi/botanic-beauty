f42f199ac435fa9f5a35ae46c89206d074607f0a
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-06-24 17:28:11 +0200

    fix: add extra allowed emails

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,10 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-const allowedEmails = ["szathrobi98@gmail.com"];
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [Google],
   callbacks: {
     signIn({ profile }) {
-      if (!allowedEmails.includes(profile?.email as string)) {
-        return false;
-      }
-
       return true;
     },
   },
