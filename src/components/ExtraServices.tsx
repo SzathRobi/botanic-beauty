b25d49215c7dc109f214921ea2cbbe5600a67fbf
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-06-25 09:12:06 +0200

    feat: change extra services icons and titles

--- src/components/ExtraServices.tsx
+++ src/components/ExtraServices.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import FadeInView from "./FadeInView";
 import Title from "./Title";
 import BackgroundBlur from "./BackgroundBlur";
-import { Crown, PawPrint, Rocket, Sun } from "lucide-react";
+import { CalendarCheck, Gem, PawPrint, Vegan } from "lucide-react";
 
 type ServiceCardProps = {
   icon: any;
@@ -26,22 +26,25 @@ const ExtraServices = () => {
         <BackgroundBlur className="mx-auto space-y-24">
           <div className="flex flex-col md:flex-row justify-between gap-10">
             <FadeInView delay={0}>
-              <ServiceCard icon={<Crown size={64} />} title="Felszabadítás" />
+              <ServiceCard
+                icon={<CalendarCheck size={64} />}
+                title="Könnyű bejelentkezés"
+              />
             </FadeInView>
 
             <FadeInView delay={0.2}>
-              <ServiceCard icon={<Sun size={64} />} title="Nappal is nyitva" />
+              <ServiceCard icon={<Vegan size={64} />} title="Vegán termékek" />
             </FadeInView>
 
             <FadeInView delay={0.4}>
-              <ServiceCard icon={<Rocket size={64} />} title="Rakéta csapat" />
+              <ServiceCard
+                icon={<PawPrint size={64} />}
+                title="Állatbarát hely"
+              />
             </FadeInView>
 
             <FadeInView delay={0.6}>
-              <ServiceCard
-                icon={<PawPrint size={64} />}
-                title="Krokodilbarát"
-              />
+              <ServiceCard icon={<Gem size={64} />} title="Kiemelt figyelem" />
             </FadeInView>
           </div>
         </BackgroundBlur>
