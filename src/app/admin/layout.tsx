a7e287aec3ee057f48c95ded5bac2eb261f380a8
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-06-25 07:35:46 +0200

    feat: make desktop admin screen ok on smaller screens

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,12 +6,12 @@ type AdminLayoutProps = {
   children: ReactNode;
 };
 
-// TODO / highest: modify and delete bookings
+// TODO / highest: modify bookings
 const AdminLayout = ({ children }: AdminLayoutProps) => {
   return (
-    <div className="min-h-screen flex gap-4 sm:gap-0 flex-col-reverse sm:flex-row pt-10">
+    <div className="min-h-screen flex gap-4 sm:gap-0 flex-col-reverse sm:flex-row pt-20">
       <AdminNav />
-      <div className="flex-1 px-2">{children}</div>
+      <div className="flex-1 px-2 pt-2">{children}</div>
     </div>
   );
 };
