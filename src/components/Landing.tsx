33fc2ed8985f212318e0f0d1f17b0a83c922ef3d
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-07-12 08:43:48 +0200

    feat: remove an image from landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -14,13 +14,6 @@ const Landing = async () => {
         priority={true}
         className="absolute -z-10 md:-top-24 object-cover"
       />
-      <Image
-        alt="arrow down"
-        src="/arrow-down.png"
-        width={128}
-        height={128}
-        className="absolute left-1/2 transform -translate-x-1/2 -z-10 -bottom-8"
-      />
       <div className="px-2 sm:px-0 flex h-full w-full flex-col items-center justify-center">
         <BackgroundBlur className="mb-24 flex flex-col items-center justify-center gap-4">
           <h1 className="text-center text-xl md:text-3xl mb-2">
