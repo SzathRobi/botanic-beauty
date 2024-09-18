55e96de18f745ce3e2f4c48db23277cf3f3bce5d
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2024-09-18 08:13:31 +0200

    feat: admin customers page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,7 +9,7 @@ type AdminLayoutProps = {
 // TODO / highest: modify bookings
 const AdminLayout = ({ children }: AdminLayoutProps) => {
   return (
-    <div className="min-h-screen flex gap-4 sm:gap-0 flex-col-reverse sm:flex-row pt-20">
+    <div className="dark min-h-screen flex gap-4 sm:gap-0 flex-col-reverse sm:flex-row pt-20">
       <AdminNav />
       <div className="flex-1 px-2 pt-4 lg:pt-16 2xl:pt-10">{children}</div>
     </div>
