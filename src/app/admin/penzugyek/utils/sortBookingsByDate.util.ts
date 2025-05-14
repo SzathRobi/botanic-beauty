7dde804544a730796c73f038a2efa1a5ab28397c
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2025-05-14 09:43:05 +0200

    fix: correct sorting in finance page

--- src/app/admin/penzugyek/utils/sortBookingsByDate.util.ts
+++ src/app/admin/penzugyek/utils/sortBookingsByDate.util.ts
@@ -1,17 +1,17 @@
 import { Booking } from '@prisma/client'
-import { compareAsc, parseISO } from 'date-fns'
+import { compareAsc } from 'date-fns'
 
 export function sortBookingsByDate(bookings: Booking[]): Booking[] {
   return bookings.sort((a, b) => {
-    // Parse selectedDate to Date objects
-    const dateA = parseISO(a.selectedDate)
-    const dateB = parseISO(b.selectedDate)
+    // Parse selectedDate to Date objects (using standard date format)
+    const dateA = new Date(a.selectedDate)
+    const dateB = new Date(b.selectedDate)
 
     // Compare the dates first
     const dateComparison = compareAsc(dateA, dateB)
     if (dateComparison !== 0) return dateComparison
 
-    // If the dates are the same, compare the time slots
+    // If the dates are the same, compare the time slots (based on the start time)
     const timeSlotA = parseTimeSlot(a.selectedTimeSlot)
     const timeSlotB = parseTimeSlot(b.selectedTimeSlot)
 
@@ -20,7 +20,7 @@ export function sortBookingsByDate(bookings: Booking[]): Booking[] {
 }
 
 function parseTimeSlot(timeSlot: string): number {
-  // Assume time slot is in the format 'HH:MM - HH:MM' (e.g. '10:00 - 11:00')
+  // Assume time slot is in the format 'HH:MM - HH:MM' (e.g. '13:30 - 18:00')
   const [startTime, endTime] = timeSlot.split(' - ').map((time) => {
     const [hours, minutes] = time.split(':').map(Number)
     return hours * 60 + minutes // Convert to total minutes
