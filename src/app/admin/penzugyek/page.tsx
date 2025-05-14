4ce43aa797ff002b63d2c23cc0eb5dfd180d8861
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2025-05-14 09:12:43 +0200

    feat: sort bookings b date on admin finance page

--- src/app/admin/penzugyek/page.tsx
+++ src/app/admin/penzugyek/page.tsx
@@ -5,6 +5,7 @@ import prisma from '@/lib/db'
 
 import { filterBookingsFromDate } from '../foglalasok/utils/filterBookingsFromDate.util'
 import FinanceTabs from './components/financeTabs'
+import { sortBookingsByDate } from './utils/sortBookingsByDate.util'
 
 const FinancePage = async () => {
   const session = await auth()
@@ -17,9 +18,11 @@ const FinancePage = async () => {
 
   const bookingsFromDate = filterBookingsFromDate(bookings, 2025, 5, 14)
 
+  const bookingsFromDateInOrder = sortBookingsByDate(bookingsFromDate)
+
   return (
     <div className="sm:container">
-      <FinanceTabs bookings={bookingsFromDate} />
+      <FinanceTabs bookings={bookingsFromDateInOrder} />
     </div>
   )
 }
