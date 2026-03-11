f1c1d46878144670704f333c5d027a56247b7d11
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2026-03-11 13:27:48 +0100

    fix: calendar offdays

--- src/app/admin/foglalasok/utils/offDay.ts
+++ src/app/admin/foglalasok/utils/offDay.ts
@@ -1,13 +1,19 @@
 import { TOffDay } from '@prisma/client'
 
+const addThreeHours = (date: Date | string) => {
+  const d = new Date(date)
+  d.setHours(d.getHours() + 3)
+  return d
+}
+
 export const isOffDayOfTimi = (
   dateToCheck: Date,
   offDays: TOffDay[]
 ): boolean => {
-  const check = dateToCheck.toLocaleDateString('hu-HU')
+  const check = addThreeHours(dateToCheck).toLocaleDateString('hu-HU')
 
   return offDays.some((offDay) => {
-    const offDate = new Date(offDay.date).toLocaleDateString('hu-HU')
+    const offDate = addThreeHours(offDay.date).toLocaleDateString('hu-HU')
 
     return offDate === check && offDay.person === 'Timi'
   })
@@ -17,10 +23,10 @@ export const isOffDayOfNemTimi = (
   dateToCheck: Date,
   offDays: TOffDay[]
 ): boolean => {
-  const check = dateToCheck.toLocaleDateString('hu-HU')
+  const check = addThreeHours(dateToCheck).toLocaleDateString('hu-HU')
 
   return offDays.some((offDay) => {
-    const offDate = new Date(offDay.date).toLocaleDateString('hu-HU')
+    const offDate = addThreeHours(offDay.date).toLocaleDateString('hu-HU')
 
     return offDate === check && offDay.person === 'nem_Timi'
   })
