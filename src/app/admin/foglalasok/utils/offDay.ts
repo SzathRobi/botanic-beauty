19bd092f282807e894eafc9140db3726d75d08e6
Szathmári Róbert <robert.szathmari@budapestkozut.hu>
2026-03-11 12:58:04 +0100

    fix: calendar offdays

--- src/app/admin/foglalasok/utils/offDay.ts
+++ src/app/admin/foglalasok/utils/offDay.ts
@@ -1,24 +1,33 @@
 import { TOffDay } from '@prisma/client'
-import { format } from 'date-fns'
 
 export const isOffDayOfTimi = (
   dateToCheck: Date,
-  OffDays: TOffDay[]
+  offDays: TOffDay[]
 ): boolean => {
-  return OffDays.some(
-    (offDay) =>
-      format(new Date(offDay.date), 'yyyy-MM-dd') ===
-        format(dateToCheck, 'yyyy-MM-dd') && offDay.person === 'Timi'
-  )
+  return offDays.some((offDay) => {
+    const offDate = new Date(offDay.date)
+
+    return (
+      offDate.getFullYear() === dateToCheck.getFullYear() &&
+      offDate.getMonth() === dateToCheck.getMonth() &&
+      offDate.getDate() === dateToCheck.getDate() &&
+      offDay.person === 'Timi'
+    )
+  })
 }
 
 export const isOffDayOfNemTimi = (
   dateToCheck: Date,
-  OffDays: TOffDay[]
+  offDays: TOffDay[]
 ): boolean => {
-  return OffDays.some(
-    (offDay) =>
-      format(new Date(offDay.date), 'yyyy-MM-dd') ===
-        format(dateToCheck, 'yyyy-MM-dd') && offDay.person === 'nem_Timi'
-  )
+  return offDays.some((offDay) => {
+    const offDate = new Date(offDay.date)
+
+    return (
+      offDate.getFullYear() === dateToCheck.getFullYear() &&
+      offDate.getMonth() === dateToCheck.getMonth() &&
+      offDate.getDate() === dateToCheck.getDate() &&
+      offDay.person === 'nem_Timi'
+    )
+  })
 }
